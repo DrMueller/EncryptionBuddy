793884aeffcfd0d79595bd3348326d260b53a2f8
Matthias Müller <matthias.mueller@trivadis.com>
2020-04-25 11:56:57 +0200

    Working 2

--- Sources/Application/Areas/Services/Implementation/EncryptionService.cs
+++ Sources/Application/Areas/Services/Implementation/EncryptionService.cs
@@ -19,93 +19,57 @@ namespace Mmu.EncryptionBuddy.Areas.Services.Implementation
         // https://stackoverflow.com/questions/1629828/how-to-encrypt-a-string-in-net
         public async Task<string> DescryptAsync(string cipherText)
         {
-            // Instantiate a new RijndaelManaged object to perform string symmetric encryption
-            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            var rijndaelCipher = new RijndaelManaged();
 
-            // Set key and IV
             rijndaelCipher.Key = ProvideKey();
             rijndaelCipher.IV = ProvideInitialVector();
 
-            // Instantiate a new MemoryStream object to contain the encrypted bytes
-            MemoryStream memoryStream = new MemoryStream();
+            var memoryStream = new MemoryStream();
+            var rijndaelDecryptor = rijndaelCipher.CreateDecryptor();
+            var cryptoStream = new CryptoStream(memoryStream, rijndaelDecryptor, CryptoStreamMode.Write);
 
-            // Instantiate a new encryptor from our RijndaelManaged object
-            ICryptoTransform rijndaelDecryptor = rijndaelCipher.CreateDecryptor();
-
-            // Instantiate a new CryptoStream object to process the data and write it to the 
-            // memory stream
-            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelDecryptor, CryptoStreamMode.Write);
-
-            // Will contain decrypted plaintext
-            string plainText = String.Empty;
+            var plainText = string.Empty;
 
             try
             {
-                // Convert the ciphertext string into a byte array
-                byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                var cipherBytes = Convert.FromBase64String(cipherText);
 
-                // Decrypt the input ciphertext string
                 cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
 
-                // Complete the decryption process
                 cryptoStream.FlushFinalBlock();
+                var plainBytes = memoryStream.ToArray();
 
-                // Convert the decrypted data from a MemoryStream to a byte array
-                byte[] plainBytes = memoryStream.ToArray();
-
-                // Convert the encrypted byte array to a base64 encoded string
                 plainText = Encoding.ASCII.GetString(plainBytes, 0, plainBytes.Length);
             }
             finally
             {
-                // Close both the MemoryStream and the CryptoStream
                 memoryStream.Close();
                 cryptoStream.Close();
             }
 
-            // Return the encrypted data as a string
             return plainText;
         }
 
         public async Task<string> EncryptAsync(string plainText)
         {
-            // Instantiate a new RijndaelManaged object to perform string symmetric encryption
-            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            var rijndaelCipher = new RijndaelManaged();
 
-            // Set key and IV
             rijndaelCipher.Key = ProvideKey();
             rijndaelCipher.IV = ProvideInitialVector();
 
-            // Instantiate a new MemoryStream object to contain the encrypted bytes
-            MemoryStream memoryStream = new MemoryStream();
+            var memoryStream = new MemoryStream();
+            var rijndaelEncryptor = rijndaelCipher.CreateEncryptor();
+            var cryptoStream = new CryptoStream(memoryStream, rijndaelEncryptor, CryptoStreamMode.Write);
+            var plainBytes = Encoding.ASCII.GetBytes(plainText);
 
-            // Instantiate a new encryptor from our RijndaelManaged object
-            ICryptoTransform rijndaelEncryptor = rijndaelCipher.CreateEncryptor();
-
-            // Instantiate a new CryptoStream object to process the data and write it to the 
-            // memory stream
-            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelEncryptor, CryptoStreamMode.Write);
-
-            // Convert the plainText string into a byte array
-            byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
-
-            // Encrypt the input plaintext string
             cryptoStream.Write(plainBytes, 0, plainBytes.Length);
-
-            // Complete the encryption process
             cryptoStream.FlushFinalBlock();
 
-            // Convert the encrypted data from a MemoryStream to a byte array
-            byte[] cipherBytes = memoryStream.ToArray();
-
-            // Close both the MemoryStream and the CryptoStream
+            var cipherBytes = memoryStream.ToArray();
             memoryStream.Close();
             cryptoStream.Close();
 
-            // Convert the encrypted byte array to a base64 encoded string
-            string cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
-
-            // Return the encrypted data as a string
+            var cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
             return cipherText;
         }
 
